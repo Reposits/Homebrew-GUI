cea366839748df68b5d16f3db895cef1e269f19d
Reposits <122860205+Reposits@users.noreply.github.com>
2023-05-28 15:51:45 +1000

    Create main.lua
    We're So Back

---
+++ main.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Reposits/Homebrew-GUI/main/loading.lua', true))();
+--useless because people can just execute the hombrew file instead, but i do like putting splash texts
